6d8a9f7486d5dbcb0008f686d879980a2bf99d1e
George Kaluwin <georgekaluwin@gmail.com>
2022-10-22 12:53:44 +1030

    page changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,14 @@ import "./css/Footer.css"
 export default function Footer()  {
   return (
     <div id='contact' className=''>
-        <form className="form">
             <div className='contact-message'>
                 <h1>Contact</h1>
-                <p className=''>Submit the form below or contact me via any of the following
+                <p className=''>Contact me via any of the following
                 </p>
-                <a href="https://www.linkedin.com/in/george-kaluwin">LinkedIn </a>
-                    <a href="mailto:georgekaluwin@gmail.com">Email</a>
-                    <a href="https://github.com/Gkal14">GitHub </a>
+                <a href="https://www.linkedin.com/in/george-kaluwin" target="_blank">LinkedIn </a>
+                    <a href="mailto:georgekaluwin@gmail.com" target="_blank">Email</a>
+                    <a href="https://github.com/Gkal14" target="_blank">GitHub </a>
             </div>
-            <input className='' type="text" placeholder='Name' name='name' />
-            <input className='' type="email" placeholder='Email' name='email' />
-            <textarea name="message" rows="10" placeholder='Message'></textarea>
-            <button className=''>Contact me</button>
-        </form>
     </div>
   )
 }
